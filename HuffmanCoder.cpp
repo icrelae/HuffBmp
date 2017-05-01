45ecd368b061771d6b2343382a5a67fb97b922df
icrelae <icrelae@gmail.com>
2017-05-01 22:52:40 +0000

    build huffman tree

--- HuffmanCoder.cpp
+++ HuffmanCoder.cpp
@@ -1,6 +1,50 @@
 #include "HuffmanCoder.h"
 
 HuffmanCoder::HuffmanCoder(const std::map<char, unsigned> &map):
-	valueWeight(map), huffmanRoot(' ', 0)
+	valueWeight(map),
+	huffmanRoot(std::make_shared<HuffTreeNode>(Pair_CU({' ', 0})))
 {
+}
+
+bool HuffmanCoder::SortByWeight(const HuffTreeNodePtr &lhs,
+		const HuffTreeNodePtr &rhs)
+{
+	return lhs->data.second < rhs->data.second;
+}
+
+HuffmanCoder::HuffTreeNodePtr HuffmanCoder::BuildTree()
+{
+	std::list<HuffmanCoder::HuffTreeNodePtr> lstNodePtr;
+	// cast 'pair<char, unsigned>' into 'HuffTreeNode'
+	for (auto const &pairValWei : valueWeight) {
+		lstNodePtr.push_back(std::make_shared<HuffTreeNode>(pairValWei));
+	}
+	lstNodePtr.sort(SortByWeight);
+	decltype(lstNodePtr)::const_iterator lstIt1, lstIt2;
+	HuffTreeNodePtr newNode;
+	while (1 < lstNodePtr.size()) {
+		// merge first two node in 'newNode' as lchild and rchild
+		lstIt1 = lstIt2 = lstNodePtr.begin();
+		++lstIt2;
+		newNode = Combine2Node(*lstIt1, *lstIt2);
+		// delete first two node in list
+		lstNodePtr.erase(lstNodePtr.begin());
+		lstNodePtr.erase(lstNodePtr.begin());
+		// find place in list to insert 'newNode'
+		lstIt1 = find_if(lstNodePtr.begin(), lstNodePtr.end(),
+				[newNode] (const HuffTreeNodePtr &p) {
+					return p->data.second > newNode->data.second;
+				});
+		lstNodePtr.insert(lstIt1, newNode);
+	}
+	return *lstNodePtr.begin();
+}
+
+HuffmanCoder::HuffTreeNodePtr HuffmanCoder::Combine2Node(
+		const HuffmanCoder::HuffTreeNodePtr &lnode,
+		const HuffmanCoder::HuffTreeNodePtr &rnode)
+{
+	return std::make_shared<HuffTreeNode>(
+			Pair_CU({' ', lnode->data.second + rnode->data.second}),
+			lnode, rnode);
 }
--- HuffmanCoder.h
+++ HuffmanCoder.h
@@ -3,6 +3,9 @@
 
 #include <map>
 #include <memory>
+#include <utility>
+#include <algorithm>
+#include <list>
 #include "coder.h"
 #include "BinaryTree.h"
 
@@ -10,13 +13,20 @@ class HuffmanCoder: public Coder {
 	public:
 		HuffmanCoder(const std::map<char, unsigned>&);
 		virtual ~HuffmanCoder();
-		virtual std::string Encode(std::string);
-		virtual std::string Decode(std::string);
+		virtual std::string Encode(std::string) const override;
+		virtual std::string Decode(std::string) const override;
+		using Pair_CU = std::pair<char, unsigned>;
+		using HuffTreeNode = BinaryTreeNode<Pair_CU>;
+		using HuffTreeNodePtr = std::shared_ptr<HuffTreeNode>;
 	private:
-		std::shared_ptr<BinaryTreeNode<paire<char, unsigned>>> huffmanRoot;
 		std::map<char, unsigned> valueWeight;
 		std::map<char, std::string> mapTable;
-		std::shared_ptr<BinaryTreeNode<paire<char, unsigned>>> BuildTree();
+		HuffTreeNodePtr huffmanRoot;
+		HuffTreeNodePtr BuildTree();
+		static bool SortByWeight(
+				const HuffTreeNodePtr&, const HuffTreeNodePtr&);
+		static HuffTreeNodePtr Combine2Node(
+				const HuffTreeNodePtr&, const HuffTreeNodePtr&);
 };
 
 #endif // _HUFFMANCODER_H
